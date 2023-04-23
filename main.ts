a0f88e66849cd744eb19246ec6d16d36e72030f6
Gazebeaux <theandrewchandler@gmail.com>
2023-04-23 15:10:15 -0500

    1
    GPT

--- main.ts
+++ main.ts
@@ -1,86 +1,30 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface TravelSystemSettings {
+	isMounted: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: TravelSystemSettings = {
+	isMounted: false
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class TravelSystemPlugin extends Plugin {
+	settings: TravelSystemSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addRibbonIcon('dice', 'Travel System', async () => {
+			const roll = this.roll2d20();
+			const outcome = this.getTravelOutcome(roll, this.settings.isMounted);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			new Notice(`Distance: ${outcome.distance} miles\nEncounter: ${outcome.encounter}`);
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new TravelSystemSettingTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -89,28 +33,45 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	roll2d20() {
+		return this.rollDice(2, 20);
+	}
+
+	rollDice(numDice, numSides) {
+		let total = 0;
+		for (let i = 0; i < numDice; i++) {
+			total += Math.floor(Math.random() * numSides) + 1;
+		}
+		return total;
+	}
+
+	getTravelOutcome(roll, isMounted) {
+		const onFootDistances = [1, 2, 3, 4, 5, 6, 7, 8];
+		const mountedDistances = [2, 4, 6, 8, 10, 12, 14, 16];
+		let distance;
+
+		if (isMounted) {
+			distance = mountedDistances;
+		} else {
+			distance = onFootDistances;
+		}
+
+		if (roll >= 2 && roll <= 5) {
+			return { distance: distance[0], encounter: 'Major combat encounter' };
+		} else if (roll >= 6 && roll <= 12) {
+			return { distance: distance[1], encounter: 'Minor combat encounter' };
+		} else if (roll >= 13 && roll <= 18) {
+			return { distance: distance[2], encounter: 'No encounter' };
+		}
+		// ... and so on
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class TravelSystemSettingTab extends PluginSettingTab {
+	plugin: TravelSystemPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: TravelSystemPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,17 +81,15 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for Travel System Plugin.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Mounted Travel')
+			.setDesc('Enable if the characters are mounted.')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.isMounted)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.isMounted = value;
 					await this.plugin.saveSettings();
 				}));
 	}
